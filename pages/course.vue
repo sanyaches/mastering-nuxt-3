c5caed387d93f0ef2aed71ba5a289ea724d063fc
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-11-27 23:50:18 +0300

    Add both client and server side error handling

--- pages/course.vue
+++ pages/course.vue
@@ -40,11 +40,23 @@
     </div>
 
     <div class="prose p-12 bg-white rounded-md w-[65ch]">
-      <NuxtPage />
+      <NuxtErrorBoundary>
+        <NuxtPage />
+        <template #error="{ error }">
+          <p>Something went wrong with displaying of the lesson: <code>{{ error }}</code></p>
+          <button class="hover:cursor-pointer bg-gray-500 text-white font-bold py-1 px-3 rounded" @click="resetError(error)">
+            Reset
+          </button>
+        </template>
+      </NuxtErrorBoundary>
     </div>
   </div>
 </template>
 
 <script setup>
 const { chapters, title } = useCourse()
+const resetError = async (error) => {
+  await navigateTo('/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3')
+  error.value = null
+}
 </script>
